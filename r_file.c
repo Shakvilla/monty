d2ac2c615d939311a4ce6f2cf8f7ab32734cdf0f
Shakvilla <abdulshakuraclement@yahoo.com>
2022-12-15 15:45:55 +0000

    added more functions

--- r_file.c
+++ r_file.c
@@ -135,3 +135,45 @@ void find_func(char *opcode, char *value, int ln, int format)
 	if (flag == 1)
 		err(3, ln, opcode);
 }
+
+/**
+ * call_fun - Calls the required function.
+ * @f: Pointer to the function that is about to be called.
+ * @op: string representing the opcode.
+ * @val: string representing a numeric value.
+ * @ln: line numeber for the instruction.
+ * @format: Format specifier. If 0 Nodes will be entered as a stack.
+ * if 1 nodes will be entered as a queue.
+ */
+void call_fun(op_func f, char *op, char *val, int ln, int format)
+{
+	stack_t *node;
+	int flag;
+	int i;
+
+	flag = 1;
+	if (strcmp(op, "push") == 0)
+	{
+		/*Checks if the number is negative and moves the val ptr*/
+		if (val != NULL && val[0] == '-')
+		{
+			val = val + 1;
+			flag = -1;
+		}
+		/*val is not a digit is the return value is 0*/
+		if (val == NULL)
+			err(5, ln);
+		for (i = 0; val[i] != '\0'; i++)
+		{
+			if (isdigit(val[i]) == 0)
+				err(5, ln);
+		}
+		node = create_node(atoi(val) * flag);
+		if (format == 0)
+			f(&node, ln);
+		if (format == 1)
+			add_to_queue(&node, ln);
+	}
+	else
+		f(&head, ln);
+}

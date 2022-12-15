07d48cffc088c3b055dd7dec5b8007340cecd121
Shakvilla <abdulshakuraclement@yahoo.com>
2022-12-15 15:23:17 +0000

    interpret_line function added

--- r_file.c
+++ r_file.c
@@ -53,3 +53,37 @@ void read_file(FILE *fd)
 	}
 	free(lineprt);
 }
+
+/**
+ * interpret_line - Separates each line into tokens to determine
+ * which function to call.
+ * @lineptr: String representing a line in a file.
+ * @line_number: Line number for the opcode.
+ * @format: Format specifier. If 0 Nodes will be entered as a stack.
+ * if 1 nodes will be entered as a queue.
+ * Return: Returns 0 if the opcode is stack. 1 if queue.
+ */
+int interpret_line(char *lineptr, int line_number, int format)
+{
+	const char *delim;
+	char *opcode;
+	char *value;
+
+	if (lineptr == NULL)
+		err(4);
+	delim = "\n ";
+	opcode = strtok(lineptr, delim);
+
+	/*hanlding blank lines*/
+	if (opcode == NULL)
+		return (format);
+	value = strtok(NULL, delim);
+
+	if (strcmp(opcode, "queue") == 0)
+		return (1);
+	else if (strcmp(opcode, "stack") == 0)
+		return (0);
+
+	find_func(opcode, value, line_number, format);
+	return (format);
+}

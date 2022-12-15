f73945a523a0159cea72d9d040938004303c4665
Shakvilla <abdulshakuraclement@yahoo.com>
2022-12-15 15:22:19 +0000

    reade_file function added

--- r_file.c
+++ r_file.c
@@ -25,3 +25,31 @@ void open_file(char *file_name)
 	/*might need to check for errors on closing a file.*/
 	fclose(fd);
 }
+
+
+/**
+ * read_file - Reads the content of a file line by line.
+ * @fd: Pointer to a file descriptor of an open file
+ */
+
+void read_file(FILE *fd)
+{
+	int line_n;
+	int format;
+	char *lineprt;
+	size_t n;
+
+	lineprt = NULL;
+	n = 0;
+	format = 0;
+
+	if (fd == NULL)
+		err(2, "file_name");
+
+	/*Getting each line in the file*/
+	for (line_n = 1; getline(&lineprt, &n, fd) != EOF; line_n++)
+	{
+		format = interpret_line(lineprt, line_n, format);
+	}
+	free(lineprt);
+}

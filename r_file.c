97d13a6078401af65c32369402b62c88eb03fa27
Shakvilla <abdulshakuraclement@yahoo.com>
2022-12-15 15:29:30 +0000

    find_func added

--- r_file.c
+++ r_file.c
@@ -87,3 +87,51 @@ int interpret_line(char *lineptr, int line_number, int format)
 	find_func(opcode, value, line_number, format);
 	return (format);
 }
+
+/**
+ * find_func - Finds the appropite function to run the opcode instructions.
+ * @opcode: The operation code, It could be push, pall, ...
+ * @value: The possible value for the operation.
+ * @ln: Line number for the opcode.
+ * @format: Format specifier. If 0 Nodes will be entered as a stack.
+ * if 1 nodes will be entered as a queue.
+ */
+void find_func(char *opcode, char *value, int ln, int format)
+{
+	int i;
+	int flag;
+
+	instruction_t func_list[] = {
+		{"push", add_to_stack},
+		{"pall", print_stack},
+		{"pint", print_top},
+		{"pop", pop_top},
+		{"nop", nop},
+		{"swap", swap_nodes},
+		{"add", add_nodes},
+		{"sub", sub_nodes},
+		{"div", div_nodes},
+		{"mul", mul_nodes},
+		{"mod", mod_nodes},
+		{"pchar", print_char},
+		{"pstr", print_str},
+		{"rotl", rotl},
+		{"rotr", rotr},
+		{NULL, NULL}
+    };
+
+    if (opcode[0] == '#')
+		return;
+	/*Iterates through list to find the right function*/
+	for (flag = 1, i = 0; func_list[i].opcode != NULL; i++)
+	{
+		/*When 0 found the right opcode*/
+		if (strcmp(opcode, func_list[i].opcode) == 0)
+		{
+			call_fun(func_list[i].f, opcode, value, ln, format);
+			flag = 0;
+		}
+	}
+	if (flag == 1)
+		err(3, ln, opcode);
+}

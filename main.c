79b36b5149f337ecb6a66c51a585cf0e413123f4
Shakvilla <abdulshakuraclement@yahoo.com>
2022-12-16 09:31:32 +0000

    fixing github merge conflicts

--- main.c
+++ main.c
@@ -1,57 +1,6 @@
 #include "monty.h"
-<<<<<<< HEAD
-<<<<<<< HEAD
-bus_t bus = {NULL, NULL, NULL, 0};
-/**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
-int main(int argc, char *argv[])
-{
-	char *content;
-	FILE *file;
-	size_t size = 0;
-	ssize_t read_line = 1;
-	stack_t *stack = NULL;
-	unsigned int counter = 0;
-
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-	file = fopen(argv[1], "r");
-	bus.file = file;
-	if (!file)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-	while (read_line > 0)
-	{
-		content = NULL;
-		read_line = getline(&content, &size, file);
-		bus.content = content;
-		counter++;
-		if (read_line > 0)
-		{
-			execute(content, &stack, counter, file);
-		}
-		free(content);
-	}
-	free_stack(stack);
-	fclose(file);
-return (0);
-}
-
-=======
-stack_t *head = NULL;
-=======
 
 global_t vglo;
->>>>>>> 929968c9f3372750e24898aee07afc1eafefe5b9
 
 /**
  * free_vglo - frees the global variables
@@ -119,19 +68,6 @@ FILE *check_input(int argc, char *argv[])
  */
 int main(int argc, char *argv[])
 {
-<<<<<<< HEAD
-	stack_t *node;
-
-	node = malloc(sizeof(stack_t));
-	if (node == NULL)
-		err(4);
-	node->next = NULL;
-	node->prev = NULL;
-	node->n = n;
-	return (node);
-}
->>>>>>> 07d48cffc088c3b055dd7dec5b8007340cecd121
-=======
 	void (*f)(stack_t **stack, unsigned int line_number);
 	FILE *fd;
 	size_t size = 256;
@@ -165,4 +101,3 @@ int main(int argc, char *argv[])
 
 	return (0);
 }
->>>>>>> 929968c9f3372750e24898aee07afc1eafefe5b9
